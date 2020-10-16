e32ae4d7e8dbf330b6c9fda0e822df0d88416c4e
Audrey Andoy <audreylandoy@gmail.com>
2020-10-16 23:35:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Array
+grocery_list = ["apples", "bananas", "grapes", "twix"]
+print(grocery_list)
+
+#indexing
+print(grocery_list[2])
+print(grocery_list[0])
+
+#foreach loop
+for i in grocery_list:
+    print(i, "2.99") 
+
+#for loop
+for i in range(len(grocery_list)):
+    print(grocery_list[i], "2.99")
+
